7922acb9212fbaeda6ed897300e0d8cf9c14448c
masfahru <imamfahrurrofi@gmail.com>
2025-05-31 15:59:25 +0700

    chore: update project name and description, add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JavaScript, JSX, TypeScript, and TSX files in the src folder
+    "./public/**/*.html", // Include all HTML files in the public folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
